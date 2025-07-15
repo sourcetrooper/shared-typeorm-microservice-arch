12b26efca4dbeb185e4b4caa65e01c1ede6dad63
Ren_Codex <reelcodex@gmail.com>
2025-07-15 08:56:11 +0200

    Project - Fixed Jest Test Matching pattern for Services

--- services/service-b/jest.config.js
+++ services/service-b/jest.config.js
@@ -1,12 +1,13 @@
 const { createDefaultPreset } = require("ts-jest");
 const tsJestTransformCfg = createDefaultPreset().transform;
-
 /** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
   transform: {
     ...tsJestTransformCfg,
   },
-  testMatch: ["src/**/__tests__/**/*.test.ts", "src/**/*.test.ts", "src/**/*.spec.ts"],
+
+  testMatch: ["**/*.test.ts", "**/*.spec.ts"],
   testPathIgnorePatterns: ["/node_modules/", "/dist/", "/build/"],
+  passWithNoTests: true,
 };
